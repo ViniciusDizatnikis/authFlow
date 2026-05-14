0ab4d0b0e76bcdaf412e2b30befc76845631b88d
ViniciusDizatnikis <vdizatnikis@gmail.com>
2026-05-13 21:50:18 -0300

    validation update

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,7 +1,5 @@
 export function isValidEmail(email) {
-
-    const regex =
-        /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     return regex.test(email);
 }
